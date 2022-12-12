0bbf6d0e25037393b85dfbba9b70c3e9e75ec57f
Mallikarjun Hanagandi <mallikarjun.h1410@gmail.com>
2022-12-12 10:49:22 +0530

    Push Notification Completed- fully working code

--- PushNotification/PushNotification/AppDelegate.swift
+++ PushNotification/PushNotification/AppDelegate.swift
@@ -6,30 +6,171 @@
 //
 
 import UIKit
-    
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+import FirebaseCore
+import FirebaseAuth
+import FirebaseMessaging
 
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+    
+    let gcmMessageIDKey = "gcm.message_id"
+   // let gcmMessageIDKey = "1:851743474169:ios:dc46cc6243f9cfa352fd28"
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-   
+        
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
+        
+        /*
+         Register for remote notifications
+         
+         Either at startup, or at the desired point in your application flow, register your app for remote notifications. Call registerForRemoteNotifications as shown:
+         */
+        
+        // START register_for_notifications
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self //for this you need to add - UNUserNotificationCenterDelegate at top (like adopting protocol)
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: { _, _ in }
+            )
+        } else {
+            let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        // END register_for_notifications
+        
+        /*
+         // Access the registration token
+         
+         By default, the FCM SDK generates a registration token for the client app instance on app launch. Similar to the APNs device token, this token allows you to send targeted notifications to any particular instance of your app.
+         
+         In the same way that Apple platforms typically deliver an APNs device token on app start, FCM provides a registration token via FIRMessagingDelegate's messaging:didReceiveRegistrationToken: method. The FCM SDK retrieves a new or existing token during initial app launch and whenever the token is updated or invalidated. In all cases, the FCM SDK calls messaging:didReceiveRegistrationToken: with a valid token
+         
+         // Set the messaging delegate
+         
+         To receive registration tokens, implement the messaging delegate protocol and set FIRMessaging's delegate property after calling [FIRApp configure]. For example, if your application delegate conforms to the messaging delegate protocol, you can set the delegate on application:didFinishLaunchingWithOptions: to itself.
+         
+         */
+        
+        //START set_messaging_delegate
+        Messaging.messaging().delegate = self //MessagingDelegate this you need to add/adopt protocol at top
+        //END set_messaging_delegate
+        
         return true
     }
+    
+    // [START refresh_token] - from MessagingDelegate protocol,
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        
+        UserDefaults.standard.set(fcmToken, forKey: "FcmToken")
+        UserDefaults.standard.synchronize()
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+    
+    // [END refresh_token]
+    
+    
+    // Receive displayed notifications for iOS 10 devices.
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification) async
+    -> UNNotificationPresentationOptions {
+        let userInfo = notification.request.content.userInfo
+        
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        
+        // ...
+        
+        // Print full message.
+        print(userInfo)
+        
+        // Change this to your preferred presentation option
+        return [[.alert, .sound]]
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse) async {
+        let userInfo = response.notification.request.content.userInfo
+        
+        // ...
+        
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        
+        // Print full message.
+        print(userInfo)
+    }
+    
+    
+    
+    /*
+    Handle silent push notifications
 
+    When sending messages with the content_available key (equivalent to APNs's content-available, the messages will be delivered as silent notifications, waking your app in the background for tasks like background data refresh. Unlike foreground notifications, these notifications must be handled via the application(_:didReceiveRemoteNotification:fetchCompletionHandler:) method.
+     */
+
+    func application(_ application: UIApplication,
+                     didReceiveRemoteNotification userInfo: [AnyHashable: Any]) async
+    -> UIBackgroundFetchResult {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+        
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        
+        // Print full message.
+        print(userInfo)
+        
+        return UIBackgroundFetchResult.newData
+    }
+    
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNs token retrieved: \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+        
+        // With swizzling disabled you must set the APNs token here.
+        // Messaging.messaging().apnsToken = deviceToken
+    }
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
--- PushNotification/PushNotification/ViewController.swift
+++ PushNotification/PushNotification/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func sendDeviceTokenToServer(_ sender: Any) {
+        
+        let fcmToken = UserDefaults.standard.object(forKey: "FcmToken")
+        
+        let headers = [
+            "content-type": "application/json",
+            "cache-control": "no-cache",
+            "SecretKey": "4767e127b1a2493d9796eee3f6830c0d", //this is for emSigner
+            "AppName": "EmsignerMobileApp",
+            "DeviceId": "\(fcmToken ?? "")" //this is for push notification
+        ]
+        let parameters = [
+            "UserName": "yes1@yopmail.com",
+            "Password": "111111"
+        ] as [String : Any]
+        
+        let postData = try!JSONSerialization.data(withJSONObject: parameters, options: [])
+        
+        let urlNew = "https://qamobapi-int.emsigner.com/api/ValidateLogin"
+        let request = NSMutableURLRequest(url: NSURL(string: urlNew)! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 60.0)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = headers
+        request.httpBody = postData as Data
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error as Any)
+            } else {
+                print(String(data: data!, encoding: .utf8)!)
+            }
+        })
+        
+        dataTask.resume()
+    }
 }
+
 
